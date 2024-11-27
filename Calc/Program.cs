3293bb91dd954bf17adab5c6f749741e79f6c576
Ramedon <info@ramed.one>
2024-11-27 15:21:34 +0500

    ✨ Solution

---
+++ Calc/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Calc
+{
+    class Calculator
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число:");
+            var firstNumber = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число:");
+            var secondNumber = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Результат: " + (firstNumber + secondNumber));
+        }
+    }
+}
---
+++ RandomGame/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace RandomGame
+{
+    class RandomGame
+    {
+        static void Main(string[] args)
+        {
+            var rnd = new Random();
+            var randNum  = rnd.Next(1, 100);
+            
+            Console.WriteLine("Угадай число от 1 до 100:");
+            
+            var userNumber = Convert.ToInt32(Console.ReadLine());
+            
+            while (userNumber != randNum)
+            {
+                if (userNumber > randNum)
+                    Console.WriteLine("Загаданное число меньше чем твоё");
+                else
+                    Console.WriteLine("Загаданное число больше чем твоё");
+            
+                Console.WriteLine("Попробуй еще раз:");
+                
+                userNumber = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Поздравляю, ты угадал!");
+            
+        }
+    }
+}
